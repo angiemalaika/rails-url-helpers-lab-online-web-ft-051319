ac53b2c74ff814d6f30e38e0b006171a59763176
Angela Mumba <malaika.mumba@gmail.com>
2019-08-09 16:31:18 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,12 +16,3 @@ class StudentsController < ApplicationController
     redirect_to student_path(@student)
   end
 end
-#   def show
-#   end
-
-#   private
-
-#     def set_student
-#       @student = Student.find(params[:id])
-#     end
-# end
